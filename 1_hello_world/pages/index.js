1900f2250700fbcc82c1c3602fd319658d3f44d6
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-25 23:46:39 -0300

    Colocando o import Link

--- 1_hello_world/pages/index.js
+++ 1_hello_world/pages/index.js
@@ -1,5 +1,7 @@
-import head from 'next/head'
-import image from 'next/image'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
 import Styles from '../styles/Home.module.css'  
 
 export default function Home() {
